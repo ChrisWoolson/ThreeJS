dcf975657398749a03d744d0c847d0714950838f
Chris Woolson <chriswoolson@Chriss-MacBook-Pro.local>
2023-09-18 14:19:10 -0700

    Finished car driving- turning could be more realistic

--- main.js
+++ main.js
@@ -1,43 +1,53 @@
 //npx vite to run
 import * as THREE from 'three';
 import {
-    helicopter,
-   //airplane,
+    car,
     objectsInit,
 } from './Game/objects.js';
 import {
     world,
     groundBody,
-    sphereBody,
+    carBody,
     createPhysicsWorld,
     moveBody,
 } from './Physics/physicsWorld.js';
 import CannonDebugger from 'cannon-es-debugger';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 export const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+export const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
+const orbitControls = new OrbitControls( camera, renderer.domElement );
 const light = new THREE.AmbientLight( 0xffffff, 10); 
 scene.add( light );
 
-camera.position.z = 80;
-camera.position.y = 30;
-camera.position.x = 5;
+ 
 
 objectsInit();
 createPhysicsWorld();
+
+camera.position.z = 5;
+camera.position.y = 5;
+camera.position.x = 5;
 const cannonDebugger = new CannonDebugger(scene, world);
 
 function syncPhysics(){
     world.fixedStep();
     cannonDebugger.update();
 
-    helicopter.position.copy(sphereBody.position);
-    helicopter.position.y-=2
-    helicopter.quaternion.copy(sphereBody.quaternion);
+    car.position.copy(carBody.position);
+    car.position.y-=2
+    car.quaternion.copy(carBody.quaternion);
+}
+
+function cameraUpdate(){
+    
+
+
+    orbitControls.target = car.position;
+    orbitControls.update();
 }
 
 function animate() {
@@ -45,7 +55,7 @@ function animate() {
 	requestAnimationFrame( animate );
     syncPhysics();
     moveBody();
-    camera.lookAt(helicopter.position);
+    cameraUpdate();
 	renderer.render( scene, camera );
 }
 
